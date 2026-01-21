881ff38e225b31d41541ad13499887064da4ad93
MahaJameel <142606907+MahaJameel@users.noreply.github.com>
2026-01-21 20:41:46 +0500

    AllocateBook Problem

---
+++ allocateBook.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;    
+
+bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages){
+    int stu = 1, pages = 0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>maxAllowedPages) return false;
+
+        if(pages + arr[i]<= maxAllowedPages){
+            pages += arr[i];
+        }
+        else{
+            stu++;
+            pages = arr[i];
+        }
+    }
+   return stu > m ? false : true;
+}
+
+int allocateBooks(vector<int> &arr, int n, int m){
+    if(m>n) return -1;
+    int sum =0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+
+    int st=0, end=sum; // rant of possible answers
+    int ans = -1;
+    while(st<=end){
+        int mid = st + (end-st)/2;
+
+        if(isValid(arr, n, m, mid)){  //left
+            ans = mid;
+            end = mid -1;  
+        }
+        else{   //right
+            st = mid +1;
+        }
+
+    }
+    return ans;
+}
+int main(){
+    vector<int> arr = {15,17,20};
+    int n=3, m=2;
+
+    cout << allocateBooks(arr, n, m) << endl;
+    return 0;
+}
